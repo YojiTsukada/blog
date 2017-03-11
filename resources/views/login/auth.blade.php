6f25a0caa20552c41073d01580932248d10799aa
YojiTsukada <Yoji@YMacBook-Air.local>
2017-03-11 23:23:08 +0900

    ログイン画面を追加

---
+++ resources/views/login/auth.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<html>
+<head>
+  <title>Login</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <link href="css/bttn.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+  <form>
+  <div class="container-fluid">
+    <div class="form-group">
+      <label for="InputID">UserID</label>
+      <input type="ID" class="form-control" id="InputID" aria-describedby="" placeholder="Enter Your ID">
+      <small id="" class="form-text text-muted"></small>
+    </div>
+    <div class="form-group">
+      <label for="InputPassword">Password</label>
+      <input type="password" class="form-control" id="InputPassword" placeholder="Enter Password">
+    </div>
+  </form>
+  <button class="bttn-unite bttn-md bttn-success">Submit</button>
+</div>
+
+
+
+</body>
+</html>
