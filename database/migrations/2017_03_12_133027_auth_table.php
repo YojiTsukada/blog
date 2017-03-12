1c682fd70c8b6b290ccf237ada3f6bfab858639e
YojiTsukada <Yoji@YMacBook-Air.local>
2017-03-12 23:52:14 +0900

    Database   マイグレーションファイル作成

--- database/migrations/2017_03_12_133027_auth_table.php
+++ database/migrations/2017_03_12_133027_auth_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AuthTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-/*
-        Schema::create('users', function (Blueprint $table) {
+        //
+        Schema::create('laravel', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('user_id');
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('created');
+            $table->string('updated');
+            $table->string('enabled');
         });
-*/
     }
 
     /**
@@ -32,6 +33,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-    //    Schema::dropIfExists('users');
+        //
+        Schema::dropIfExists('laravel');
+
     }
 }
